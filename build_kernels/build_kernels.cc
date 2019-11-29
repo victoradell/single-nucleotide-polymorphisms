40ab9fdec418e466bd58b5ff306c3c6829f00565
Víctor Adell <victor.adell@hotmail.com>
2019-11-29 18:08:26 +0100

    Codi R acabat, funció n-gram a C++ i canvi d'ordenacio als arxius csv

--- build_kernels/build_kernels.cc
+++ build_kernels/build_kernels.cc
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cmath>
 #include <vector>
 #include <string>
 
 using namespace std;
 
-using MatrixInt = vector<vector<int>>;
-using MatrixDouble = vector<vector<double>>;
+using MatrixInt = vector<vector<int> >;
+using MatrixDouble = vector<vector<double> >;
 
 const int N = 500;
 const int D = 39108;
+const int dictionary_length = 3; // All SNP's are codified as {0, 1, 2}
 
 
 MatrixInt load_dataframe(string filepath) {
@@ -57,7 +59,61 @@ MatrixDouble dirac_kernel(const MatrixInt& X) {
 }
 
 
+MatrixInt n_gram_features(const MatrixInt& X, int n) {
+    int features_dimension = (int)pow(dictionary_length, n);
+    cout << "Feature space has dimension " << features_dimension << endl;
+    MatrixInt features(N, vector<int>(features_dimension, 0));
+    for (int individual = 0; individual < N; ++individual) {
+        for (int starting_index = 0; starting_index <= D-n; ++starting_index) {
+            // We take advantage of the representation in base 3 (dictionary length).
+            int value = 0;
+            for (int it = 0; it < n; ++it) {
+                value += X[individual][starting_index + it] * (int)pow(dictionary_length, n-it-1);
+            }
+            ++features[individual][value];
+        }
+    }
+    return features;
+}
+
+
+MatrixInt n_gram_kernel(const MatrixInt& X, int n) {
+    MatrixInt features = n_gram_features(X, n);
+    int n_features = features[0].size();
+    cout << "Features finished: " << features.size() << " " << features[0].size() << endl;
+    
+    MatrixInt K(N, vector<int>(N));
+    for (int i = 0; i < N; ++i) {
+        if (i % 20 == 0) {
+            cout << "Building row " << i << "..." << endl;
+        }
+        for (int j = i; j < N; ++j) {
+            int total = 0;
+            for (int it = 0; it < n_features; ++it) {
+                total += features[i][it] * features[j][it];
+            }
+            K[i][j] = total;
+            K[j][i] = K[i][j];
+        }
+    }
+    return K;
+}
+
+
 void write_kernel_matrix(const MatrixDouble& K, string filepath) {
+    ofstream outfile(filepath);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (j != 0) outfile << " ";
+            outfile << K[i][j];
+        }
+        outfile << endl;
+    }
+    outfile.close();
+}
+
+
+void write_kernel_matrix(const MatrixInt& K, string filepath) {
     ofstream outfile(filepath);
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -73,11 +129,22 @@ void write_kernel_matrix(const MatrixDouble& K, string filepath) {
 int main() {
     string filepath = "../data/snp.csv";
     MatrixInt X = load_dataframe(filepath);
+    cout << "Finished loading the dataset" << endl;
 
-    MatrixDouble K = dirac_kernel(X);
+    // MatrixInt F = n_gram_features(X, 3);
+
+    MatrixInt K = n_gram_kernel(X, 11);
     cout << "Finished building K" << endl;
-
-    filepath = "../kernel_matrices/dirac.txt";
+    
+    filepath = "../kernel_matrices/11_gram.txt";
     write_kernel_matrix(K, filepath);
     cout << "Kernel matrix written at " << filepath << endl;
+
+
+    // MatrixDouble K = dirac_kernel(X);
+    // cout << "Finished building K" << endl;
+
+    // filepath = "../kernel_matrices/dirac.txt";
+    // write_kernel_matrix(K, filepath);
+    // cout << "Kernel matrix written at " << filepath << endl;
 }
--- kernel_methods/main.R
+++ kernel_methods/main.R
@@ -1,10 +1,21 @@
-library(kernlab)
+#################### Load the kernel matrix and libraries #################### 
 
-## Load the kernel matrix
-K <- as.matrix(read.csv("../kernel_matrices/dirac.txt", header=F, sep=" "))
+
+
+library(kernlab)
+library(ggplot2)
+
+K <- as.matrix(read.csv("../kernel_matrices/11_gram.txt", header=F, sep=" "))
+# K <- K / (max(K) / 2)
 K <- as.kernelMatrix(K)
 N <- dim(K)[1]
 population <- as.factor(as.matrix(read.csv("../data/population.csv", header=F)))
+
+
+
+#################### kSVM #################### 
+
+
 
 ## First we should split the data into learning (2/3) and test (1/3) parts
 ntrain <- round(N*2/3)      # number of training examples
@@ -34,6 +45,21 @@ table (pred=y1, truth=population[-tindex])
 cat('Error rate = ', 100*sum(y1 != population[-tindex])/length(y1), '%')
 
 
-#############################################
 
+#################### kPCA #################### 
+
+
+
+kpc.snp <- kpca (K, features=2, kernel="matrix")
+data <- as.data.frame(kpc.snp@rotated[,1:2])
+colnames(data) <- c("x", "y")
+data$population <- population
+
+MyPalette <- c(EthA = "black", EthE = "blue", EthK = "green", EthP = "red")
+
+g2 <- ggplot(data, aes(x=x, y=y, color=population)) + geom_point(alpha=.4)
+g2 + geom_vline(xintercept=0, linetype="dotted") + geom_hline(yintercept=0, linetype="dotted") + 
+  labs(title="2-dimensional kPCA with 11-gram Kernel") +
+  theme(plot.title = element_text(hjust = 0.5)) + theme(text=element_text(family="Ubuntu")) +
+  scale_colour_manual(values = MyPalette)
 
